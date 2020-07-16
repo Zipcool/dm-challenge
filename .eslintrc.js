b779e0d5dc1e8e0b3b676b305976d5a9adca105a
Marcos Righi Brasil <marcos_righi@yahoo.com.br>
2020-07-16 19:57:28 -0300

    Adding and applying linting rules to project.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,59 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/essential",
+        "@vue/standard"
+    ],
+    parserOptions: {
+        parser: "babel-eslint"
+    },
+    plugins: [
+        "vue"
+    ],
+    rules: {
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+        "curly": [
+            "error", 
+            "all"
+        ],
+        "indent": [
+            "error", 
+            4, 
+            { "SwitchCase": 1 }
+        ],
+        "keyword-spacing": [
+            "error", 
+            { 
+                "before": true, 
+                "after": true 
+            }
+        ],
+        "quotes": [
+            "error", 
+            "double"
+        ],
+        "semi": [
+            "error", 
+            "never"
+        ],
+        "vue/html-indent": [
+            "error", 
+            4
+        ]
+    },
+    overrides: [
+        {
+            files: [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)"
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
 }
