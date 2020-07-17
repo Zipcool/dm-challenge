5a4f369670c605daaead96fab41bef3787d03aba
Marcos Righi Brasil <marcos_righi@yahoo.com.br>
2020-07-17 00:51:36 -0300

    Adding axios and lodash.

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,34 +16,18 @@ module.exports = {
     rules: {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-        "curly": [
-            "error", 
-            "all"
-        ],
-        "indent": [
-            "error", 
-            4, 
-            { "SwitchCase": 1 }
-        ],
-        "keyword-spacing": [
-            "error", 
-            { 
-                "before": true, 
-                "after": true 
-            }
-        ],
-        "quotes": [
-            "error", 
-            "double"
-        ],
-        "semi": [
-            "error", 
-            "never"
-        ],
-        "vue/html-indent": [
-            "error", 
-            4
-        ]
+        "curly": ["error", "all"],
+        "indent": ["error", 4, { 
+            "SwitchCase": 1 
+        }],
+        "keyword-spacing": ["error", { 
+            "before": true, 
+            "after": true 
+        }],
+        "quotes": ["error", "double"],
+        "semi": ["error", "never"],
+        "space-before-function-paren": ["error", "never"],
+        "vue/html-indent": ["error", 4]
     },
     overrides: [
         {
